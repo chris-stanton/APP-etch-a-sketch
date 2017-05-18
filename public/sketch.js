aa8266ba15428424191de4e1987e5d81f0ea0ca2
Chris Stanton <cstanton0760@yahoo.com>
2017-05-18 00:05:19 -0500

    cleaned up code

--- public/sketch.js
+++ public/sketch.js
@@ -1,51 +1,45 @@
+
 $(document).ready(function() {
 
+	grid();
+	hover();
+
 	function grid() {
-		//alert('Making grid...');
-		for (var i = 0; i < 2000; i++) {
-			if(i % 200 == 0) {
-				$("#container").append("<div class='square2'></div>");
-			}
-			else {
-				$("#container").append("<div class='square'></div>");
-			};
-		};
-	};
+		for (var i = 0; i < 11178; i++) {
+			$("#container").append("<span class='square'></span>");
+		};//end for
+	};//end of grid()
 
 	function hover() {
 		$(".square").hover (
 		function() {
 			$(this).css("background-color", 'black');
 		});
-	};
+	};//end of hover()
 
-	function multi_hover() {
-		$(".square").unbind();
-		$(".square").mouseenter (
-		function() {
-			var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
-			$(this).css("background-color", hue);
-		});
-	};
+	$("#black").click(function() {
+		$("#container").children().remove('span');
+			grid();
+			hover();
+	});//end of button click
 
-
-	grid();
-	hover();
+	$("#chase").click(function() {
+		$("#container").children().remove('span');
+			grid();
+			hover();
+				$(".square").unbind();
+				$(".square").hover(function() {
+					$(this).css("opacity", 0);
+				}, function () {
+					$(this).fadeTo(1500, 1);
+				});
+	});//end of button click
 
 	$("#reset").click(function() {
 		console.log('reset');
-		$("#container").children().remove('div');
-		grid();
-		hover();
-	});
-
-	$("#snake").click(function() {
-		$(".square").unbind();
-		$(".square").hover(function() {
-			$(this).css("opacity", 0);
-		}, function () {
-			$(this).fadeTo(1500, 1);
-		});
-	});
+		$("#container").children().remove('span');
+			grid();
+			hover();
+	});//end of button click
 
 });//end of doc.ready
